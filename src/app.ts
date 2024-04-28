d4681dca5beb5bb079ad7c3401034a8210046123
John-Thailand <naoki.honda@mirrorfit.jp>
2024-04-28 20:14:46 +0900

    Google Mapによる地図の表示

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,46 @@
+import axios from 'axios';
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'API_KEY';
+
+type GoogleGeocodingResponse = {
+    results: { geometry: { location: { lat: number, lng: number } } }[]
+    status: 'OK' | 'ZERO_RESULTS'
+};
+
+declare var google: any;
 
 function searchAddressHandler(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
 
-    // Google API に送信
+    axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`).then(response => {
+        if (response.data.status !== 'OK') {
+            throw new Error('座標を取得できませんでした。');
+        }
+        const coordinates = response.data.results[0].geometry.location;
+        let map: google.maps.Map;
+        //@ts-ignore
+        const { Map } = await google.maps.importLibrary("maps");
+        map = new Map(document.getElementById("map") as HTMLElement, {
+            center: coordinates,
+            zoom: 16,
+        });
+
+        // //@ts-ignore
+        // const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+        // // The marker, positioned at Uluru
+        // new AdvancedMarkerView({
+        //     map: map,
+        //     position: coordinates,
+        //     title: "Uluru",
+        // });
+    }).catch(err => {
+        alert(err.message);
+        console.log(err);
+    });
 }
 
 form.addEventListener('submit', searchAddressHandler);
